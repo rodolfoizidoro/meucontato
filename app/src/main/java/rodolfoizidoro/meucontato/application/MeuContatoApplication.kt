72ab80622b10cb8972151e5930eca80de072d84f
Rodolfo Izidoro <rodolfo.morais@getninjas.com.br>
2019-02-04 00:03:35 -0200

    Add button login with google

---
+++ app/src/main/java/rodolfoizidoro/meucontato/application/MeuContatoApplication.kt
@@ -0,0 +1,14 @@
+package rodolfoizidoro.meucontato.application
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+import rodolfoizidoro.meucontato.di.AppModule.Companion.MeuContatoModules
+
+class MeuContatoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin(this, listOf(MeuContatoModules))
+    }
+}
---
+++ app/src/main/java/rodolfoizidoro/meucontato/di/AppModule.kt
@@ -0,0 +1,9 @@
+package rodolfoizidoro.meucontato.di
+
+import org.koin.dsl.module.module
+
+class AppModule {
+    companion object {
+        val MeuContatoModules = module {}
+    }
+}
