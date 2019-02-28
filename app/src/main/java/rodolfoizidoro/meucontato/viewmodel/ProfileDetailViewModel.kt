c4d3c1e5f78e5f64ff65d6c759136490c4a84865
Rodolfo Izidoro <rodolfo.morais@getninjas.com.br>
2019-02-28 20:26:05 -0300

    Minor imrpvoments

--- app/src/main/java/rodolfoizidoro/meucontato/viewmodel/ProfileDetailViewModel.kt
+++ app/src/main/java/rodolfoizidoro/meucontato/viewmodel/ProfileDetailViewModel.kt
@@ -9,27 +9,32 @@ import rodolfoizidoro.meucontato.model.core.Profile
 
 class ProfileDetailViewModel(private val repository: ProfileDetailRepository) : CoroutineViewModel() {
 
-    private val loadSuccess = MutableLiveData<Profile>()
-    private val saveSuccess = MutableLiveData<Void>()
-    private val loadError = MutableLiveData<Exception>()
-    private val saveError = MutableLiveData<Exception>()
+    private val mLoadSuccess = MutableLiveData<Profile>()
+    private val mSaveSuccess = MutableLiveData<Void>()
+    private val mLoadError = MutableLiveData<Exception>()
+    private val mSaveError = MutableLiveData<Exception>()
+    private val mProgress = MutableLiveData<Boolean>()
 
-    fun loadSucess() = loadSuccess as LiveData<Profile>
-    fun saveSuccess() = saveSuccess as LiveData<Void>
-    fun loadError() = loadError as LiveData<Exception>
-    fun saveError() = saveError as LiveData<Exception>
+    fun loadSucess() = mLoadSuccess as LiveData<Profile>
+    fun saveSuccess() = mSaveSuccess as LiveData<Void>
+    fun loadError() = mLoadError as LiveData<Exception>
+    fun saveError() = mSaveError as LiveData<Exception>
+    fun progress() = mProgress as LiveData<Boolean>
 
     fun loadInfo(id: String) {
         jobs add launch {
             try {
+                mProgress.value = true
                 val contacts = repository.loadContacts(id).await()
                 val profile = repository.loadProfile(id).await()
                 profile.profileContacts = contacts
 
-                loadSuccess.value = profile
+                mLoadSuccess.value = profile
 
             } catch (e: Exception) {
-                loadError.value = e
+                mLoadError.value = e
+            } finally {
+                mProgress.value = false
             }
         }
     }
@@ -37,9 +42,9 @@ class ProfileDetailViewModel(private val repository: ProfileDetailRepository) : 
     fun saveProfile() {
         jobs add launch {
             try {
-               saveSuccess.value = repository.saveProfile(loadSucess().value!!).await()
+                mSaveSuccess.value = repository.saveProfile(loadSucess().value!!).await()
             } catch (e: Exception) {
-                saveError.value = e
+                mSaveError.value = e
             }
         }
     }
